ff89b4ffb61b64af985aa81e1f12328c6d9fee44
Loi Van Tran <datloiboi@gmail.com>
2021-07-07 11:33:50 +0700

    leveled up our MVP

---
+++ languages.js
@@ -0,0 +1,20 @@
+const languages = [
+  'ar',
+  'zh',
+  'en',
+  'es',
+  'fr',
+  'ru',
+]
+
+
+
+function renderLanguageAnchorTags() {
+  // const languageHTML = languages.map(l => l)
+  const languageHTML = languages.map(
+    (l) => `<a href="http://127.0.0.1:5500/index.html?language=${l}">${l}</a>`,
+  );
+  document.getElementById("languages").innerHTML = languageHTML
+}
+
+renderLanguageAnchorTags()
--- news.js
+++ news.js
@@ -20,6 +20,25 @@ function rendersArticles(articles) {
 }
 
 
+function produceUrl() {
+  const foo = window.location.search
+    .split("?")[1]
+    .split("&")
+    .map((qP) => {
+      const [key, value] = qP.split("=");
+      return {
+        key: key,
+        value: value,
+      };
+    });
+
+    let url =
+      "https://newsapi.org/v2/top-headlines?apiKey=dsadsv";
+    console.log({ foo });
+    return foo.map((p) => url + `&${p.key}=${p.value}`).join('');
+}
+
+
 
 async function fetchArticles() {
   // NOTES : Singature of fetch 1st argument url, 2nd is configuration obj
@@ -43,16 +62,27 @@ async function fetchArticles() {
 
 
   // 2. Async/Await 
-  try {
-    const resp = await fetch(
-      "https://newsapi.org/v2/top-headlines?apiKey=6eec2f7fe6cd4c40a3fef8f33f5778fe&language=en&category=health",
-    );
 
+  let url = produceUrl();
+
+  console.log({url});
+
+
+
+  // Sometimes internet bad
+  let articles = []
+  try {
+    const resp = await fetch(url);
     const json = await resp.json();
     console.log({ article: json.articles[0], json });
-    rendersArticles(json.articles)
+    articles = json.articles
+    localStorage.setItem("willNotWork", articles);
+    localStorage.setItem("willWork", JSON.stringify(articles));
   } catch (error) {
     console.log({error, foo: 'bar', spam: 'ham'});
+    articles = JSON.parse(localStorage.getItem("willWork"));
+  } finally {
+    rendersArticles(articles)
   }
 }
 
